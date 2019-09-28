b79d18f928bff9d18a9b4f75dc704f33efd4a1d4
Miftachulwijati <48273349+Miftachulwijati@users.noreply.github.com>
2019-09-28 13:22:29 +0700

    Add files via upload

--- Miftachul_Wijati_5A_17090020/nama.sql
+++ Miftachul_Wijati_5A_17090020/nama.sql
@@ -1,4 +1,32 @@
-CREATE TABLE `data_pesantren` (
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 28, 2019 at 08:13 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nama`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_pasien`
+--
+
+CREATE TABLE `data_pasien` (
   `id` int(10) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `username` varchar(30) NOT NULL,
@@ -7,22 +35,24 @@ CREATE TABLE `data_pesantren` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `data`
+-- Dumping data for table `data_pasien`
 --
 
-INSERT INTO `data_pesantren` (`id`, `nama`, `username`, `password`, `email`) VALUES
-(1, 'Nadiatul Wahidah', 'nadia', 'dia1', 'dia1@gmail.com'),
-(2, 'Dian Febriani', 'Dian', 'an23', 'dian@gmail.com'),
-(4, 'Tiara Arista', 'Tiara', 'tiara45', 'tiara@gmail.com');
+INSERT INTO `data_pasien` (`id`, `nama`, `username`, `password`, `email`) VALUES
+(1, 'Melly Setiani', 'Melly', 'melly03', 'melly03@gmail.com'),
+(2, 'Yumn Khaulah', 'Yumn', 'yum16', 'yumn16@gmail.com'),
+(4, 'Intan Rina', 'intan', 'intan05', 'intan05@gmail.com'),
+(5, 'ulvi jujul', 'ulvi', 'ulvi06', 'ulvi06@gmail.com'),
+(7, 'Aqila Syafia ', 'Aqila', 'Aqila23', 'Aqila23@gmail.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `data`
+-- Indexes for table `data_pasien`
 --
-ALTER TABLE `data_pesantren`
+ALTER TABLE `data_pasien`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -30,12 +60,10 @@ ALTER TABLE `data_pesantren`
 --
 
 --
--- AUTO_INCREMENT for table `data`
+-- AUTO_INCREMENT for table `data_pasien`
 --
-ALTER TABLE `data_pesantren`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
+ALTER TABLE `data_pasien`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
